728794f5021c42c9a5398912c32e985aa0a9d08c
JeauLabyorteaux <jeau711@gmail.com>
2026-05-16 21:53:56 -0700

    added icon for back button

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-// 1. SELECTING ELEMENTS
-// We use 'document' (the global object for the page) to find elements.
-// This is similar to finding an object in a list or a node in a tree.
 const body = document.body;
 const cards = document.querySelectorAll('.section-card, .project-card');
 const home_c3po = document.getElementById('c3po');
-const c3po_home = document.getElementById('c3po-back');
 
+// Light / Dark Mode Toggle Button
 const theme_toggle_button = document.createElement('button');
 theme_toggle_button.className = "theme-toggle";
 theme_toggle_button.innerHTML = `<img src="theme.png" class="mode-icon" alt="Toggle Theme">`;
 body.prepend(theme_toggle_button);
 
+// C3PO->Home Button
+const c3po_home = document.createElement('button');
+c3po_home.className = "theme-toggle";
+c3po_home.innerHTML = `<img src="back.png" class="back-icon" alt="Toggle Theme">`;
+
+
+
+// Light / Dark Mode Event Listener
 theme_toggle_button.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
 
-// Home -> C3PO
+
+
+// Home -> C3PO Event Listener
 if(home_c3po){
-home_c3po.addEventListener('click',function(){
-    window.location.href = 'c3po.html';
+    home_c3po.addEventListener('click',function(){
+        window.location.href = 'c3po.html';
 })
 
 }
 
-// C3PO -> Home
+// C3PO -> Home Event Listener
 if(c3po_home){
-c3po_home.addEventListener('click',function(){
-    window.location.href = 'index.html';
+    body.prepend(c3po_home);
+    c3po_home.addEventListener('click',function(){
+        window.location.href = 'index.html';
 })
 }
+
