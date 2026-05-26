b70fefe2c02f12e2c7e9f6009aba6eb1d62b9be2
JeauLabyorteaux <jeau711@gmail.com>
2026-05-25 22:42:52 -0700

    refactor into neater files

--- js/script.js
+++ js/script.js
@@ -8,18 +8,18 @@ const home_movie = document.getElementById('movie_project');
 // Light / Dark Mode Toggle Button
 const theme_toggle_button = document.createElement('button');
 theme_toggle_button.className = "theme-toggle";
-theme_toggle_button.innerHTML = `<img src="../assets/images/theme.png" class="mode-icon" alt="Toggle Theme">`;
+theme_toggle_button.innerHTML = `<img src="/assets/images/theme.png" class="mode-icon" alt="Toggle Theme">`;
 body.prepend(theme_toggle_button);
 
 // C3PO->Home Button
 const c3po_home = document.createElement('button');
 c3po_home.className = "theme-toggle";
-c3po_home.innerHTML = `<img src="../assets/images/back.png" class="back-icon" alt="Toggle Theme">`;
+c3po_home.innerHTML = `<img src="/assets/images/back.png" class="back-icon" alt="Toggle Theme">`;
 
 // Score_Sender->Home Button
 const ss_home = document.createElement('button');
 ss_home.className = "theme-toggle";
-ss_home.innerHTML = `<img src="../assets/images/back.png" class="back-icon" alt="Toggle Theme">`;
+ss_home.innerHTML = `<img src="/assets/images/back.png" class="back-icon" alt="Toggle Theme">`;
 
 // Light / Dark Mode Event Listener
 theme_toggle_button.addEventListener('click', function() {
@@ -31,7 +31,7 @@ theme_toggle_button.addEventListener('click', function() {
 // Home -> C3PO Event Listener
 if(home_c3po){
     home_c3po.addEventListener('click',function(){
-        window.location.href = '/html/c3po.html';
+        window.location.href = 'c3po.html';
 })
 
 }
@@ -39,7 +39,7 @@ if(home_c3po){
 // Home -> Score Sender Event Listener
 if(home_ss){
     home_ss.addEventListener('click',function(){
-        window.location.href = '/html/score_sender.html';
+        window.location.href = 'score_sender.html';
 })
  
 }
@@ -47,7 +47,7 @@ if(home_ss){
 // Home -> Movie Event Listener
 if(home_movie){
     home_movie.addEventListener('click',function(){
-        window.location.href = '/html/movie_search.html';
+        window.location.href = 'movie_search.html';
 })
 
 }
@@ -56,7 +56,7 @@ if(home_movie){
 if(document.title === "C3PO"){
     body.prepend(c3po_home);
     c3po_home.addEventListener('click',function(){
-        window.location.href = '../index.html';
+        window.location.href = 'index.html';
 })
 }
 
@@ -64,7 +64,7 @@ if(document.title === "C3PO"){
 if(document.title === "Score Automation Project"){
     body.prepend(ss_home);
     ss_home.addEventListener('click',function(){
-        window.location.href = '../index.html';
+        window.location.href = 'index.html';
 })
 }
 
